bfcebc54374c2520f3cfadfc6bce0bbc7f062ade
suraj.be <suraj.be@media.net>
2024-05-23 22:41:06 +0530

    add conditional check for no articles

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -4,6 +4,10 @@ import NewsCard from "./NewsCard";
 export default function NewsList() {
   const { filteredArticles } = useNewsContext();
 
+  if (!filteredArticles.length) {
+    return <p className='no-articles-message'>Sorry! No articles found</p>;
+  }
+
   return (
     <ul className='news-card-wrap'>
       {filteredArticles.map((article) => (
