620199d5292b7875bd591ccd748afbfbbd668634
suraj.be <suraj.be@media.net>
2024-05-23 22:38:26 +0530

    filter block css styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import { IoFilterSharp } from "react-icons/io5";
+import { MdOutlineCancel } from "react-icons/md";
 import "./assets/styles/index.scss";
 import Filters from "./components/Filters";
 import NewsList from "./components/NewsList";
 
 function App() {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+  function handleFilterToggle() {
+    setIsFilterOpen((prev) => !prev);
+  }
+
   return (
     <>
       <div className='root-wrapper'>
         <div className='container'>
+          <header className='header'>
+            <button className='header-filter-icon' onClick={handleFilterToggle}>
+              {isFilterOpen ? <MdOutlineCancel /> : <IoFilterSharp />}
+            </button>
+          </header>
           <div className='news-content'>
-            <div className='left-wrap'>
-              <Filters />
+            <div className={`left-wrap ${isFilterOpen ? "filter-block-visible" : ""}`}>
+              {<Filters />}
             </div>
             <div className='right-wrap'>
               <NewsList />
