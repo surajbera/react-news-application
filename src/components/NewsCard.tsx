87103681ac544bb69bccbab1d57fb59b5ab9f0b0
suraj.be <suraj.be@media.net>
2024-05-23 15:46:08 +0530

    newscard component css

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -9,20 +9,22 @@ interface NewsCardProps {
 export default function NewsCard({ article }: NewsCardProps) {
   return (
     <li className='news-card'>
-      <div className='top-wrap'>
-        <figure className='img-wrap'>
-          <img src={getImageUrl(article.image)} alt={article.title} />
-        </figure>
-        <div className='content-wrap'>
-          <div className='date-wrap'>
-            <span className='date'>{article.date}</span>
+      <article>
+        <div className='top-wrap'>
+          <figure className='img-wrap'>
+            <img src={getImageUrl(article.image)} alt={article.title} />
+          </figure>
+          <div className='content-wrap'>
+            <div className='date-wrap'>
+              <span className='date'>{article.date}</span>
+            </div>
+            <h3 className='title article-title two-line'>{article.title}</h3>
           </div>
-          <h3 className='title article-title two-line'>{article.title}</h3>
+          <p className='category'>{article.source}</p>
         </div>
-      </div>
-      <p className='desc article-desc three-line'>{removeHtmlTags(article.body)}</p>
-      <p className='author'>{article.author}</p>
-      <p className='category'>{article.source}</p>
+        <p className='desc article-desc three-line'>{removeHtmlTags(article.body)}</p>
+        <p className='author'>{article.author}</p>
+      </article>
     </li>
   );
 }
